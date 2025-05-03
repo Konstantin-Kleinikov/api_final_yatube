9612573092c3629b322e53db71955743664f0ff0
Констатин Клейников <kkleinikov@yandex.ru>
2025-05-03 21:38:11 +0300

    1.1 Added Djoser for user authentication and Simple JWT support. Isort configured.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+
+urlpatterns_v1 = [
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
+]
 
 urlpatterns = [
+    path('v1/', include(urlpatterns_v1)),
 ]
